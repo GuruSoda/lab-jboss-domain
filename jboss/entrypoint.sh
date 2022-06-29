5ef751b8b42a3a072ab415d31fa719f809ca2e22
Fabricio D. Rossi <fabricio.rossi@gmail.com>
2022-06-29 00:56:25 -0300

    Nueva version del laboratorio de jboss * Se mejoraron todos los componenetes. * Se independizaron los nombres.

--- jboss/entrypoint.sh
+++ jboss/entrypoint.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
-# service ssh start
+# /opt/td-agent/embedded/bin/fluentd -c /etc/fluent/fluent.conf -d /var/run/td-agent/fluentd.pid -v
+
+if [ "$FLUENTD" = "1" ]; then
+  /opt/td-agent/bin/fluentd -c /etc/fluent/fluent.conf --no-supervisor -v &
+  sleep 5
+fi
 
 IP=`hostname -I`
 HOSTNAME=`hostname`
 
-# ./jboss-cli.sh --connect --controller=master --user=admin --password=admin@2020 --command=':read-attribute(name=product-version)'
+# ./jboss-cli.sh --connect --controller=master --user=admin --password=admin@2022 --command=':read-attribute(name=product-version)'
 
 if [ -f "/mnt/share/domain.xml" ]; then
   cp /mnt/share/domain.xml /opt/jboss/domain/configuration/ 
@@ -19,28 +24,33 @@ if [ -f "/mnt/share/host-slave.xml" ]; then
   cp /mnt/share/host-slave.xml /opt/jboss/domain/configuration/ 
 fi 
 
+if [ ! -d "/opt/jboss/domain/log" ]; then
+  mkdir "/opt/jboss/domain/log"
+fi
+
 if [ $1 = "master" ]; then
-	cd $JBOSS_HOME/bin && ./domain.sh --host-config=host-master.xml -Djboss.domain.base.dir=/opt/jboss/domain/ -Djboss.bind.address.management=$IP -Djboss.hostname=$HOSTNAME;
+	cd $JBOSS_HOME/bin && ./domain.sh --host-config=host-master.xml -Djboss.domain.base.dir=/opt/jboss/domain/ -Djboss.bind.address.management=$IP -Djboss.hostname=$HOSTNAME >> /opt/jboss/domain/log/console.log
 elif [ $1 = "slave" ]; then
 	cd $JBOSS_HOME/bin
 
-	 while sleep 1; do ./jboss-cli.sh --connect --controller=master --user=admin --password=admin@2020 --command=':read-attribute(name=product-version)'
+	 while sleep 1; do ./jboss-cli.sh --connect --controller=master --user=admin --password=admin@2022 --command=':read-attribute(name=product-version)' >> /opt/jboss/domain/log/console.log
 		 PROCESS_1_STATUS=$?
 		 if [ $PROCESS_1_STATUS -eq 0  ]; then
 			 break
 		 fi
 	done
 	
-	./domain.sh --host-config=host-slave.xml -Djboss.domain.base.dir=/opt/jboss/domain/ --backup --cached-dc -Djboss.domain.master.address=master -Djboss.domain.backup.address=backup -Djboss.bind.address.management=$IP -Djboss.bind.address=$IP -Djboss.hostname=$HOSTNAME;
+	./domain.sh --host-config=host-slave.xml -Djboss.domain.base.dir=/opt/jboss/domain/ --backup --cached-dc -Djboss.domain.master.address=master -Djboss.domain.backup.address=backup -Djboss.bind.address.management=$IP -Djboss.bind.address=$IP -Djboss.hostname=$HOSTNAME >> /opt/jboss/domain/log/console.log
 elif [ $1 = "bash" ]; then
 	/bin/bash;
 elif [ $1 = "cli" ]; then
 	cd $JBOSS_HOME/bin && ./jboss-cli.sh;
 elif [ $1 = "init" ]; then
-	cd $JBOSS_HOME/bin && ./jboss-cli.sh --connect --controller=master --user=admin --password=admin@2020 --file=/opt/repositorio/init-dominio.txt;
+	cd $JBOSS_HOME/bin && ./jboss-cli.sh --connect --controller=master --user=admin --password=admin@2022 --file=/opt/repositorio/init-dominio.txt;
 	echo "Todo Bien?"
 elif [ $1 = "test" ]; then
         echo "esto es un test en " $IP;
 else
 	/bin/bash;
 fi
+
--- laboratorio.sh
+++ laboratorio.sh
@@ -1,32 +1,32 @@
 #!/bin/sh
 
 # para tener un desktop en la red
-# docker run -it --rm --name desktop-credi --hostname desktop -p 5901:5901 -p 6901:6901 --network jbossnet www.planetaguru.com.ar/desktop-credi bash
+# docker run -it --rm --name desktop --hostname desktop -p 5901:5901 -p 6901:6901 --network jbossnet www.planetaguru.com.ar/desktop bash
 # docker create --name desktop --hostname desktop -p 5901:5901 -p 6901:6901 --network jbossnet www.planetaguru.com.ar/desktop
 
 case "$1" in
   'construir')
     cd jboss
-    docker build . -t www.planetaguru.com.ar/jboss-eap-7.2.0-credi
+    docker build . -t www.planetaguru.com.ar/jboss-eap-7.3.0
     cd ..
     cd apache
-    docker build . -t www.planetaguru.com.ar/apache-credi
+    docker build . -t www.planetaguru.com.ar/apache
     cd ..
     cd haproxy
-    docker build . -t www.planetaguru.com.ar/haproxy-credi
+    docker build . -t www.planetaguru.com.ar/haproxy
     cd ..
     ;;
   'crear') 
     docker network create jbossnet
 
-    docker create --name host-master  --hostname master  --network jbossnet -p 9990:9990 -v `pwd`/jboss:/mnt/share www.planetaguru.com.ar/jboss-eap-7.2.0-credi master
-    docker create --name host-backup  --hostname backup  --network jbossnet -v `pwd`/jboss:/mnt/share www.planetaguru.com.ar/jboss-eap-7.2.0-credi slave 
-    docker create --name host-slave01 --hostname slave01 --network jbossnet -v `pwd`/jboss:/mnt/share www.planetaguru.com.ar/jboss-eap-7.2.0-credi slave
-    docker create --name host-slave02 --hostname slave02 --network jbossnet -v `pwd`/jboss:/mnt/share www.planetaguru.com.ar/jboss-eap-7.2.0-credi slave
-    docker create --name host-slave03 --hostname slave03 --network jbossnet -v `pwd`/jboss:/mnt/share www.planetaguru.com.ar/jboss-eap-7.2.0-credi slave
-    docker create --name apache01     --hostname apache01  --network jbossnet www.planetaguru.com.ar/apache-credi
-    docker create --name apache02     --hostname apache02  --network jbossnet www.planetaguru.com.ar/apache-credi
-    docker create --name haproxy      --hostname haproxy  --network jbossnet -v `pwd`/haproxy/haproxy.cfg:/etc/haproxy/haproxy.cfg:ro -v `pwd`/haproxy/custom_404.html:/etc/haproxy/errors/custom_404.html:ro -p 80:80 www.planetaguru.com.ar/haproxy-credi
+    docker create --name host-master  --hostname master  --network jbossnet -p 9990:9990 -v `pwd`/jboss:/mnt/share www.planetaguru.com.ar/jboss-eap-7.3.0 master
+    docker create --name host-backup  --hostname backup  --network jbossnet -v `pwd`/jboss:/mnt/share www.planetaguru.com.ar/jboss-eap-7.3.0 slave
+    docker create --name host-slave01 --hostname slave01 --network jbossnet -v `pwd`/jboss:/mnt/share www.planetaguru.com.ar/jboss-eap-7.3.0 slave
+    docker create --name host-slave02 --hostname slave02 --network jbossnet -v `pwd`/jboss:/mnt/share www.planetaguru.com.ar/jboss-eap-7.3.0 slave
+    docker create --name host-slave03 --hostname slave03 --network jbossnet -v `pwd`/jboss:/mnt/share www.planetaguru.com.ar/jboss-eap-7.3.0 slave
+    docker create --name apache01     --hostname apache01  --network jbossnet www.planetaguru.com.ar/apache
+    docker create --name apache02     --hostname apache02  --network jbossnet www.planetaguru.com.ar/apache
+    docker create --name haproxy      --hostname haproxy  --network jbossnet -v `pwd`/haproxy/haproxy.cfg:/etc/haproxy/haproxy.cfg:ro -v `pwd`/haproxy/custom_404.html:/etc/haproxy/errors/custom_404.html:ro -p 80:80 www.planetaguru.com.ar/haproxy
     ;;
   'iniciar')
     docker start host-master host-backup host-slave01 host-slave02 host-slave03 apache01 apache02 haproxy 
@@ -43,13 +43,13 @@ case "$1" in
     docker network remove jbossnet
     ;;
   'cli')
-    docker run -it --rm --name jboss-cli --hostname jboss-cli --network jbossnet -v `pwd`/repositorio:/opt/repositorio www.planetaguru.com.ar/jboss-eap-7.2.0-credi bash
+    docker run -it --rm --name jboss-cli --hostname jboss-cli --network jbossnet -v `pwd`/repositorio:/opt/repositorio www.planetaguru.com.ar/jboss-eap-7.3.0 bash
     ;;
   'firefox')
     docker run -d --rm --name firefox --hostname firefox --network jbossnet -v `pwd`/repositorio:/opt/repositorio --env PUID=1000 --env PGID=1000 --env TZ=America/Argentina/Buenos_Aires --shm-size 1gb --link haproxy:jbosslab -p 3300:3000 lscr.io/linuxserver/firefox
     ;;
   'init')
-    docker run -it --rm --name jboss-init --hostname jboss-init --network jbossnet -v `pwd`/repositorio:/opt/repositorio www.planetaguru.com.ar/jboss-eap-7.2.0-credi init 
+    docker run -it --rm --name jboss-init --hostname jboss-init --network jbossnet -v `pwd`/repositorio:/opt/repositorio www.planetaguru.com.ar/jboss-eap-7.3.0 init
     ;;
   'reset')
     ./laboratorio.sh detener ; ./laboratorio.sh borrar ; ./laboratorio.sh construir ; ./laboratorio.sh crear ; ./laboratorio.sh iniciar
